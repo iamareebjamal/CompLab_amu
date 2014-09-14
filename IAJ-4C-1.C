2650478a2525d8e6370ad890339823ef0003c88c
iamareebjamal <jamal.areeb@gmail.com>
2014-09-14 15:54:42 +0530

    Menu Driven Program Update

--- IAJ-4C-1.C
+++ IAJ-4C-1.C
@@ -9,19 +9,22 @@
 
 void main(){
 
-	int choice, a, b, x;
+	int choice, a, b;
 	clrscr();
 
 	printf("Choose one option:\n1. Add two numbers\n2. Check the number is even or odd\n3. Calcuate remainder of 2 integers\n4. Execute the expression : x = ++a - --b\n5. Print your name\n\n");
 	scanf("%d", &choice);
+	printf("\n\t\t\t========================");
 
 	switch(choice){
 	 case 1 :
+		printf("\n\n\t\t\t   Sum of two integers");
 		printf("\n\nEnter two numbers\n");
 		scanf("%d%d", &a, &b);
-		printf("\nSum of two numbers %d and %d is %d", a,b, a + b);
+		printf("\nSum of two numbers %d and %d is %d", a, b, a + b);
 		break;
 	 case 2 :
+		printf("\n\n\t\t\t       Even or Odd");
 		printf("\n\nEnter a number:\n");
 		scanf("%d", &a);
 		if(a%2==0){
@@ -31,21 +34,24 @@ void main(){
 		}
 		break;
 	 case 3 :
+		printf("\n\n\t\t\tRemainder of two integers");
 		printf("\n\nEnter two numbers:\n");
 		scanf("%d%d", &a, &b);
 		printf("\n\nRemainder of the two integers %d and %d is : %d", a, b, a%b);
 		break;
 	 case 4 :
+		printf("\n\n\t\t\t   Solving expression");
 		printf("\n\nEnter two number a and b:\n");
 		scanf("%d%d", &a, &b);
-		x = ++a - --b;
-		printf("\n\nThe result of x = ++a - --b:\n=>x = %d", x);
+		printf("\n\nThe result of x = ++a - --b:\n=> x = %d", ++a - --b);
 		break;
-	 case 5 :
-		printf("\n\nMy name is Areeb Jamal");
 	 default:
-		break;
-
+		if(choice > 5|| choice < 1){
+			printf("\n\nEnter choice between 1 and 5...");
+			break;
+		}
+		printf("\n\n\t\t\t     Print your name");
+		printf("\n\nMy name is Areeb Jamal");
 
 	}
 
