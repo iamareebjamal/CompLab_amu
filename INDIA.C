e4dffd1156651636ce319f9b69a5a78861a7db98
iamareebjamal <jamal.areeb@gmail.com>
2014-11-09 20:09:27 +0530

    Finalised Patriotism

--- INDIA.C
+++ INDIA.C
@@ -8,20 +8,19 @@
 #include <conio.h>
 
 //Ye Mera India
-//For best reults, don'tuse Turbo C(Adam used that compiler, you are in 2014)
-//Use Eclipse, CodeBlocks, CodeLite or any other recent C compiler
+//Let's play a game! Try to understand the code below and  try to guess the output by hint above.
+//Let the game begin
 
 void main(){
 	int a,b,c, count = 1;
 	clrscr();
-	for (b=c=10; a="- FIGURE?, UMKC,XYZHello Folks,\
-TFy!QJu ROo TNn(ROo)SLq SLq ULo+\
-UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\
-NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
-HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
-T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\
-Hq!WFs XDt!" [b+++21]; )
-	for( ; a--> 64 ; )
-	putchar(+c=='Z' ? c=c/9:33^b&1);
+	for (b=c=10; a="iamareebjamal-\0Secret\0Encoding\0Services,Try%%to%%guess%%the%%code:\
+				TFy!QJu ROo TNn(ROo)SLq SLq ULo+UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^NBELPeHBFH\
+				T}TnALVlBLOFAkHFOuFETpHCStHAUFAgcEAelclcn^r^r\\tZvYxXyT|S~Pn SPm SOn TNn ULo0ULo#ULo-WHq!WFs XDt!" [b+++60]; )
+		for( ; a--> 64 ; )
+			putchar(+c=='Z' ? c=c/9:33^b&1);
 	getch();
 }
+
+//Don't use Baba Adam time compilers like Turbo C to print the result
+//Use Eclipse, CodeBlocks, CodeLite or any other recent C Compiler
