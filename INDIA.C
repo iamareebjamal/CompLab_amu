e75a2e1ad53da95736c6f94b7d0142d6443f53ff
iamareebjamal <jamal.areeb@gmail.com>
2014-11-09 19:03:12 +0530

    Patriotism

---
+++ INDIA.C
@@ -0,0 +1,27 @@
+/*
+ * Areeb Jamal
+ * A1A - 049 (First Year) COBT
+ * 2014-15
+ */
+
+#include <stdio.h>
+#include <conio.h>
+
+//Ye Mera India
+//For best reults, don'tuse Turbo C(Adam used that compiler, you are in 2014)
+//Use Eclipse, CodeBlocks, CodeLite or any other recent C compiler
+
+void main(){
+	int a,b,c, count = 1;
+	clrscr();
+	for (b=c=10; a="- FIGURE?, UMKC,XYZHello Folks,\
+TFy!QJu ROo TNn(ROo)SLq SLq ULo+\
+UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\
+NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
+HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
+T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\
+Hq!WFs XDt!" [b+++21]; )
+	for( ; a--> 64 ; )
+	putchar(+c=='Z' ? c=c/9:33^b&1);
+	getch();
+}
